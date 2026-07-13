6df18482bae733876b35880a388659e9a5945475
Chris Bayliss <christopher.bayliss@unimelb.edu.au>
2026-07-13 15:42:21 +1000

    feat: updates database connection pool creation
    Previously, the PostgreSQL connection pool was supposed to be acquired via a helper function that threw an error if the
    pool had not been instansiated. This changes the logic so that the pool is always created whenever the `src/lib/db.ts`
    is imported. The `geyConnection` helper function still exists for compatibility but simply returns the pool variable.
    
    For improved flexibility the connection pool now can also accept `node-postgres`'s default libpq behaviour as well as
    accepting a URI style conneciton string via PG_URL. The `NodeJS.ProcessEnv` type is augmented so the various environment
    variables potentially in use are now included in the type definition for improved type safety.
    
    This isn't the cleanest way to handle this. Ideally errors thrown when querying the database would result in an error
    in the web page and missing configuration would throw an error on startup.

--- README.md
+++ README.md
@@ -29,8 +29,18 @@ To learn more about Next.js, take a look at the following resources:
 
 You can check out [the Next.js GitHub repository](https://github.com/vercel/next.js) - your feedback and contributions are welcome!
 
-## Deploy on Vercel
+## Config
 
-The easiest way to deploy your Next.js app is to use the [Vercel Platform](https://vercel.com/new?utm_medium=default-template&filter=next.js&utm_source=create-next-app&utm_campaign=create-next-app-readme) from the creators of Next.js.
-
-Check out our [Next.js deployment documentation](https://nextjs.org/docs/app/building-your-application/deploying) for more details.
+To configure the PostreSQL database connection either use:
+```dotenv
+PG_URL="postgres://<user>:<password>@<host>:<port>/<database>"
+```
+or
+```dotenv
+PG_user=<user>
+PG_password=<password>
+PG_host=<host>
+PG_port=<port>
+PG_database=<database>
+```
+or the libpq configuration as [described here](https://www.postgresql.org/docs/14/libpq-envars.html)
--- src/lib/db.ts
+++ src/lib/db.ts
@@ -1,9 +1,17 @@
-import { Pool } from 'pg';
+import * as process from "node:process"
+import {Pool, PoolConfig} from 'pg';
 
-let connection: Pool | undefined;
+let config: PoolConfig | undefined
 
-if (!connection) {
-    connection = new Pool({
+if (process.env.PG_URL) {
+    // Using a 'postgres://' URI
+    const connectionString = process.env.PG_URL
+    console.debug("Got \"PG_URL\" %s", connectionString)
+    config = {connectionString}
+} else if (process.env.PG_user && process.env.PG_host && process.env.PG_database && process.env.PG_password) {
+    // Old environment variables
+    console.debug("Using original environment variables");
+    config = {
         user: process.env.PG_user,
         host: process.env.PG_host,
         database: process.env.PG_database,
@@ -12,14 +20,58 @@ if (!connection) {
         ssl: {
             rejectUnauthorized: false,
         }
-    });
+    }
+} else {
+    console.debug("Using node-postgresql config logic");
 }
 
+/**
+ * The connection pool object for accessing PostgreSQL. Configured automatically when imported
+ * by environment variables. Either by  "PG_URL" containing a `postgresql://` style connection string,
+ * the default libpq options {@link https://www.postgresql.org/docs/current/libpq-envars.html}, or the
+ * `PG_*` environment variables used in previous versions.
+ *
+ * @example
+ * ```
+ * PG_URL="postgres://user:password@example/database"
+ * ```
+ *
+ * @example
+ * ```
+ * PG_host="example"
+ * PG_database="database"
+ * PG_username="user"
+ * PG_password="password"
+ * ```
+ *
+ * @example
+ * ```
+ * PGHOST="example"
+ * PGDATABASE="database"
+ * PGUSER="user"
+ * PGPASSWORD="password"
+ * ```
+ *
+ */
+export const connection: Pool = new Pool(config);
+
+// the pool will emit an error on behalf of any idle clients
+// it contains if a backend error or network partition happens
+connection.on('error', (err) => {
+    console.error('Unexpected error on idle client', err);
+    process.exit(-1);
+});
+
+connection.on('connect', () => {
+    console.debug("PostgreSQL connection created");
+});
+
+/**
+ * @return the postgresql connection pool object
+ * @deprecated import {@link connection} directly instead
+ */
 export function getConnection(): Pool {
-    if (!connection) {
-        throw new Error("Database connection is undefined");
-    }
-    return connection;
+    return connection
 }
 
 export default connection;
